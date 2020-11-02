2b017ca30a8f1dde2d68e6c8a65e4c524ca36e3e
Simon Walton <saw29@hotmail.co.uk>
2020-11-02 13:04:23 +0000

    set up the routes for the projects with the paths for each page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import WFP from "./pages/WFP";
+import Methods from "./pages/Methods";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/about" exact component={() => <About />} />
+          <Route path="/contact" exact component={() => <Contact />} />
+          <Route path="/services" exact component={() => <Services />} />
+          <Route path="/wfp" exact component={() => <WFP />} />
+          <Route path="/methods" exact component={() => <Methods />} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
