09f20ddd5e322bec2c0d6e477b0cb8c4f97853fc
Simon Walton <saw29@hotmail.co.uk>
2020-11-03 19:39:28 +0000

    updated color scheme throughout project

--- src/constants/styleConstants.js
+++ src/constants/styleConstants.js
@@ -1,6 +1,8 @@
 export const colors = {
   mainBlue: '#5FC7FF',
-  darkBlue: '#0095D6',
+  darkBlue: '#0071ad',
   grey: '#E5E5E5',
-  lightBlue: '#D2E8FF'
+  lightBlue: '#d5f0ff',
+  green: '#5fffe7',
+  purple: '#5f77ff'
 };
