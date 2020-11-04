79714811102f52d4ced3db3cf2f6e5b451a97f3d
Simon Walton <saw29@hotmail.co.uk>
2020-11-04 09:58:41 +0000

    updated NAVIGATION logic so that the active nav has the .active className

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,57 +17,28 @@ const navMargin = select('& a', {
 });
 
 class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isActive: false,
-    };
-  }
-
-  getClassName() {
-    if (this.state.isActive) {
-      return 'active';
-    }
-    return null;
-  }
-
-  handlePage = () => {
-    const pathname = window.location.pathname;
-    if (pathname === '/about') {
-      this.setState({ isActive: true });
-    } else {
-      this.setState({ isActive: false });
-    }
-  };
-
   render() {
-  return (
-    <Navbar collapseOnSelect expand="lg" {...navStyle}>
-      <Navbar.Brand href="/">
-        <img src={Logo} alt="PureClean Logo" width={180} />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className={`mr-auto ${navMargin}`}>
-          <Nav.Link href="/about" className={this.getClassName()}>
-            About
-          </Nav.Link>
-          <Nav.Link href="/services">Services</Nav.Link>
-          <Nav.Link href="/methods">Methods</Nav.Link>
-          <Nav.Link href="/wfp">WFP</Nav.Link>
-          <Nav.Link href="/contact">Contact US</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-  }
-   componentDidMount() {
-    window.addEventListener('load', this.handlePage);
+    const pathname = window.location.pathname;
+    return (
+      <Navbar collapseOnSelect expand="lg" {...navStyle}>
+        <Navbar.Brand href="/">
+          <img src={Logo} alt="PureClean Logo" width={180} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className={`mr-auto ${navMargin}`}>
+          {pathname === "/about" ? <Nav.Link href="/about" className="active">About</Nav.Link> : <Nav.Link href="/about">About</Nav.Link>}
+          {pathname === "/services" ? <Nav.Link href="/services" className="active">Services</Nav.Link> : <Nav.Link href="/services">Services</Nav.Link>}
+          {pathname === "/methods" ? <Nav.Link href="/methods" className="active">Methods</Nav.Link> : <Nav.Link href="/methods">Methods</Nav.Link>}
+          {pathname === "/wfp" ? <Nav.Link href="/wfp" className="active">WFP</Nav.Link> : <Nav.Link href="/wfp">WFP</Nav.Link>}
+          {pathname === "/contact" ? <Nav.Link href="/contact" className="active">Contact Us</Nav.Link> : <Nav.Link href="/contact">Contact Us</Nav.Link>}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
   }
 }
 
 export default Navigation;
-
-
 
 
