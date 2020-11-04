3a505b31ed791d9ed88b08f86aaf8ae8e9bf230a
Simon Walton <saw29@hotmail.co.uk>
2020-11-04 16:28:02 +0000

    began work on layout of SERVICES page. Have sectioned into tabs and have  abstracted each tab's content

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import { Tabs, Tab } from "react-bootstrap";
 import { css } from "glamor";
 import { colors } from "../constants/styleConstants";
+import { tabTitles } from "../constants/tabTitles";
+import SoftfitTab from "../servicesTabs/SoftfitTab";
+import GutterTab from "../servicesTabs/GutterTab";
+import SolarTab from "../servicesTabs/SolarTab";
+import CladdingTab from "../servicesTabs/CladdingTab";
+import ConservatoryTab from "../servicesTabs/ConservatoryTab";
+import Check from "../images/check.svg";
 
 const tabStyle = css({
-  backgroundColor: colors.lightBlue,
+  backgroundColor: colors.darkBlue,
   padding: '8px',
+  color: 'white',
   '& a': {
     padding: '16px auto',
     margin: '0 auto',
@@ -14,12 +22,11 @@ const tabStyle = css({
   '& .nav-link.active': {
     color: colors.darkBlue,
     backgroundColor: 'white',
-    borderColor: colors.lightBlue
+    borderColor: colors.green
   },
-
     borderBottom: 'none',
   '& a:hover': {
-    color: colors.darkBlue
+    color: 'white'
   },
   '@media(max-width: 700px)': {
     display: 'block',
@@ -31,26 +38,51 @@ const tabStyle = css({
   }
 });
 
+const checkList = css({
+  display: 'inline-block',
+  '& li': {
+    fontFamily: 'Bebas Neue',
+    listStyleImage: `url(${Check})`,
+    float: 'left',
+    marginRight: '40px'
+  },
+  '@media(max-width: 992px)': {
+    display: 'block',
+    '& li': {
+      float: 'none'
+    }
+  }
+});
+
 const Services = () => {
   return(
-    <div {...css({backgroundColor: colors.mainBlue})}>
-    <Tabs defaultActiveKey="softfit" id="services-tab" {...tabStyle}>
-      <Tab eventKey="softfit" title="Softfit & Fascia  Cleaning">
-        <p>foo</p>
-      </Tab>
-      <Tab eventKey="gutter" title="Gutter cleaning">
-        <p>foo</p>
-      </Tab>
-      <Tab eventKey="solar" title="Solar Panels">
-        <p>foo</p>
-      </Tab>
-      <Tab eventKey="cladding" title="Cladding Cleaning">
-        <p>foo</p>
-      </Tab>
-      <Tab eventKey="conservatory" title="Conservatory Roof Cleaning">
-        <p>foo</p>
-      </Tab>
-    </Tabs>
+    <div>
+      <Tabs defaultActiveKey="softfit" id="services-tab" {...tabStyle}>
+        <Tab eventKey="softfit" title={tabTitles.softfit}>
+          <SoftfitTab />
+        </Tab>
+        <Tab eventKey="gutter" title={tabTitles.gutter}>
+          <GutterTab />
+        </Tab>
+        <Tab eventKey="solar" title={tabTitles.solar}>
+          <SolarTab />
+        </Tab>
+        <Tab eventKey="cladding" title={tabTitles.cladding}>
+          <CladdingTab />
+        </Tab>
+        <Tab eventKey="conservatory" title={tabTitles.conservatory}>
+          <ConservatoryTab />
+        </Tab>
+      </Tabs>
+      <div {...css({backgroundColor:colors.grey, display:'flex', justifyContent: 'center',  padding: '16px', borderTop: `2px solid ${colors.green}`})}>
+         <ul {...checkList}>
+            <li>Professional and reliable service</li>
+            <li>Fully insured</li>
+            <li>Improve the look of your property</li>
+            <li>Regular or one off cleans</li>
+            <li>Water fed pole system</li>
+          </ul>
+      </div>
     </div>
   );
 }
