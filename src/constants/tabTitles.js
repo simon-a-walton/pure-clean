b93c55e1e7431ee3a2d1f5d6583f5ec07afaa426
Simon Walton <saw29@hotmail.co.uk>
2020-11-04 16:31:02 +0000

    have put the tab titles in the constants folder as they appear in several places throughout the app

---
+++ src/constants/tabTitles.js
@@ -0,0 +1,7 @@
+export const tabTitles = {
+  softfit: "Softfit & Fascia Cleaning",
+  gutter: "Gutter Cleaning",
+  solar: "Solar Panels",
+  cladding: "Cladding Cleaning",
+  conservatory: "Conservatory Roof Cleaning"
+};
