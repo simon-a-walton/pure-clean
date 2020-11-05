6dfc0fdbe4dcbd0d8bacda2dc4ae5c6b87b8bee8
Simon Walton <saw29@hotmail.co.uk>
2020-11-05 16:12:54 +0000

    updated the background color to include a linear gradient and added the company logo onto the bottom of a text container

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,33 +2,19 @@ import React from "react";
 import { css } from "glamor";
 import { colors } from "../constants/styleConstants";
 import ContactForm from "../components/ContactForm";
-import { bottomBorder } from "../constants/styleRules";
-
-const gridContainer = css({
-  display: 'grid',
-  gridTemplateColumns: '60% 40%',
-  padding: '40px 100px',
-  '@media(max-width: 992px)': {
-    display: 'block',
-    padding: '40px',
-    '& div': {
-      margin: '24px 0'
-    }
-  }
-});
+import { gridContainer } from "../constants/styleRules";
+import Logo from "../images/logo.jpg";
 
 const Contact = () => {
   return (
-    <div {...css({backgroundColor: colors.darkBlue, color: 'white'})}>
+    <div {...css({backgroundImage: `linear-gradient(45deg, ${colors.darkBlue}, ${colors.mainBlue})`, color: 'white'})}>
 
       <div {...gridContainer}>
         <div>
-          <h4 >
-        <span {...bottomBorder}>GET IN TOUCH</span>
-      </h4>
+
           <ContactForm />
         </div>
-        <div {...css({margin: 'auto', textAlign: 'center'})}>
+        <div {...css({margin: 'auto', textAlign: 'center', '@media(max-width: 992px)':{marginTop:'24px'}})}>
           <iframe
             maxWidth="100%"
             height="300"
@@ -41,9 +27,26 @@ const Contact = () => {
             {...css({border: '4px solid white'})}
           >
           </iframe>
-          <div {...css({padding: '16px', margin: '0 auto', textAlign: 'left'})}>
-            <p><i className="fas fa-mobile-alt" /> Paul: 07702 113360</p>
-            <p><i class="fas fa-at" /> purecleanwindow@outlook.com</p>
+          <div {...css({color: 'black', padding: '16px', backgroundColor: 'white', borderRadius:'8px', margin: '20px auto', '& a:hover': {color: colors.darkBlue}})}>
+            <img
+              src={Logo}
+              width={200}
+              alt="logo"
+            />
+            <div {...css({padding:'12px'})}>
+              <a href="tel:07702 113360" target="_blank" rel="noreferrer">
+                <h6>
+                  <i className="fas fa-mobile-alt" />
+                  &nbsp; Paul: 07702 113360
+                </h6>
+              </a>
+              <a href="mailto:purecleanwindow@outlook.com" target="_blank" rel="noreferrer">
+                <h6>
+                  <i className="fas fa-at" />
+                  &nbsp; purecleanwindow@outlook.com
+                </h6>
+              </a>
+            </div>
           </div>
         </div>
       </div>
