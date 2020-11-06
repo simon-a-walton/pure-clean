cc96c683334d17837b279bebee1ac8821c34fcdf
Simon Walton <saw29@hotmail.co.uk>
2020-11-06 15:42:10 +0000

    updated slightly the naming of the tab titles

--- src/constants/tabTitles.js
+++ src/constants/tabTitles.js
@@ -1,6 +1,6 @@
 export const tabTitles = {
-  softfit: "Softfit & Fascia Cleaning",
-  gutter: "Gutter Cleaning",
+  softfit: "Soffit, Gutter & Fascia Cleaning",
+  gutter: "Gutter Clearing",
   solar: "Solar Panels",
   cladding: "Cladding Cleaning",
   conservatory: "Conservatory Roof Cleaning"
